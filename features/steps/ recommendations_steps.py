a6d7201eba7a82e9257e276240851166ca91beed
garyleelty <tl3285@nyu.edu>
2022-11-21 17:33:19 -0500

    add steps and environment

---
+++ features/steps/ recommendations_steps.py
@@ -0,0 +1,26 @@
+import requests
+from behave import given
+from compare import expect
+
+@given('the following recommendations')
+def step_impl(context):
+    """ Delete all Recommendations and load new ones """
+    # List all of the recommendations and delete them one by one
+    rest_endpoint = context.BASE_URL + '/recommendations'
+    context.resp = requests.get(rest_endpoint)
+    expect(context.resp.status_code).to_equal(200)
+    for recommendation in context.resp.json():
+        context.resp = requests.delete(rest_endpoint + '/' + str(recommendation['id']))
+        expect(context.resp.status_code).to_equal(204)
+    # load the database with the new recommendations
+    for row in context.table:
+        payload = {
+            "id": row['id'],
+            "name": row['name'],
+            "recommendation_id": row['recommendation_id'],
+            "recommendation_name": row['recommendation_name'],
+            "type": row['type'],
+            "number_of_likes": row['number_of_likes']
+        }
+        context.resp = requests.post(rest_endpoint, json=payload)
+        expect(context.resp.status_code).to_equal(201)
